9142d60cbaf7930422450045238ea7c340424ec5
giacomolombardi2 <catlexion595r@gmail.com>
2020-05-20 18:02:48 +0200

    Ci

---
+++ ElencoFilm/ElencoFilm/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ElencoFilm.Models;
+using Xamarin.Essentials;
+
+namespace ElencoFilm
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Films Archivio { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Archivio = new Films(); //crea un archivio di film
+            Archivio.Load(); //carica i film
+            DatiView.ItemsSource = Archivio; //collega Archivio con la datiview
+        }
+
+        private void Add_click(object sender, EventArgs e) //aggiunge all archivio un film con nome, link e immagine
+        {
+            Archivio.Add(new Film { Name = "Giacomo", Link = "Http://www.youtube.com", Imagine = "" });
+        }
+
+        private void Save_click(object sender, EventArgs e) // si salva il film
+        {
+            Archivio.Save();
+        }
+
+        async private void Video_click(object sender, EventArgs e) //apre il link
+        {
+            // Il record corrente passato con . , è dentro all'attributo "CommandParameter"
+            // il quale è dentro al Button...
+            // Button ci arriva come object e va fatto un cast con l'operatore as
+            // Se l'operatore as torna null, qualcosa è andato storto... meglio non fare nulla
+            Button B = sender as Button;
+            if (B != null)
+            {
+                // Anche CommandParameter è un object e serve un cast a Film
+                Film FM = B.CommandParameter as Film;
+                if (FM != null)
+                {
+                    await Browser.OpenAsync(FM.Link, BrowserLaunchMode.SystemPreferred);
+                }
+            }
+        }
+
+    }
+}
---
+++ ElencoFilm/ElencoFilm/Models/Film.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace ElencoFilm.Models
+{
+    public class Film
+    {
+        public string Imagine { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Films : ObservableCollection<Film>
+    {
+        public Films() 
+        {
+          
+        }
+
+        public void Load() //apre il file e carica i film
+        {
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                string NomeDelFile = Path.Combine(path, "films.csv");
+
+                if (File.Exists(NomeDelFile))
+                {
+                    StreamReader r = new StreamReader(NomeDelFile);
+                    while( !r.EndOfStream)
+                    {
+                        string riga = r.ReadLine();
+                        string[] dati = riga.Split(';');
+                        Film FM = new Film { Name = dati[0], Imagine = dati[1], Link = dati[2] };
+                        Add(FM);
+                    }
+                    r.Close();
+                }
+            }
+            catch (Exception errore) { }
+
+        }
+        public void Save() //salva i film nel file
+        {
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                string NomeDelFile = Path.Combine(path, "films.csv");
+
+                StreamWriter w = new StreamWriter(NomeDelFile);
+
+                foreach ( Film FM in this )
+                {
+                    w.WriteLine( $"{FM.Name};{FM.Imagine};{FM.Link}" );
+                }
+                w.Close();
+            }
+            catch (Exception errore) { }
+
+        }
+
+    }
+}
